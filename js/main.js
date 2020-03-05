3880915ee5a4e5b48585b309c56874b20b5b9a71
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-03-05 10:39:16 +0200

    The first commit after reinstalling Windows.

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$( document ).ready(
+    function(){
+        if ($('.home__slider_list')) {
+            $('.home__slider_list').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                fade: true,
+                speed: 2000,
+            });
+        }
+        if ($('.home__portfolio_slider_list')) {
+            //custom function showing current slide
+            let $status = $('.home__portfolio_slider_pagination');
+            let $slickElement = $('.home__portfolio_slider_list');
+
+            $('.home__portfolio_slider_list').slick({
+                infinite: false,
+                // dots: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow: $('.home__portfolio_slider_prev'),
+                nextArrow: $('.home__portfolio_slider_next'),
+                // customPaging : function() {
+                //     $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+                //         //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+                //         let slideNumber = '0' + ((currentSlide ? currentSlide : 0) + 1);
+                //         $status.text(slideNumber + '/' + slick.slideCount);
+                //         for (let i = 0; i < slideCount; i++) {
+                //             $status.text(i + '/' + slick.slideCount);
+                //             let span = $('<span class="home__portfolio_slider_pagination_item"></span>').text(slideNumber);
+                //             $status.append(span);
+                //         }
+                //     });
+                // },
+            });
+        }
+    }
+);
