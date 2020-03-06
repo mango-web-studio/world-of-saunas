934a275cbdf1736dc7360ddff0d6fc9381f24b4c
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-03-06 10:51:27 +0200

    Fixed the tasks manager (gulp).

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,56 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    tinypng      = require('gulp-tinypng-compress'),
-    iconfont     = require('gulp-iconfont'),
-    iconfontCss  = require('gulp-iconfont-css'),
-    fontName     = 'myfont',
-	browserSync  = require('browser-sync'),
-	autoprefixer = require('gulp-autoprefixer');
+// Initialize modules
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 
-gulp.task('sass', function(){
-	return gulp.src('scss/**/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(sass({outputStyle: 'expanded'}))
-	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-	.pipe(gulp.dest('css'))
-	.pipe(browserSync.reload({stream: true}))
-});
+var paths = {
+    styles: {
+        src: './scss/**/*.scss',
+        dest: './css/'
+    },
+    scripts: {
+        src: './js/**/*.js',
+        dest: './scripts/'
+    },
+    html: {
+        src: './*.html',
+        dest: './'
+    }
+};
 
-gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: './'
-		},
-		notify: false
-	});
-});
 
-gulp.task('tinypng', function() {
-    gulp.src('img/**/*.{png,jpg,jpeg}')
-	.pipe(tinypng({
-		key: 'xMgdrYZJjl2bhB8jZ8LK5tWWQmGRkP2N',
-		sigFile: 'img/.tinypng-sigs',
-		sameDest: true,
-		log: true,
-		summarize: true
-	}))
-	.pipe(gulp.dest('img'));
-});
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.styles.dest));
+}
 
-gulp.task('iconfont', function(){
-	// var runTimestamp = Math.round(Date.now()/1000);
-	
-	gulp.src(['img/svg/*.svg'])
-	.pipe(iconfontCss({
-	  fontName: fontName,
-	  path: 'scss/templates/_icons.scss',
-	  targetPath: '../../scss/_icons.scss',
-	  fontPath: '../fonts/iconsfont/'
-	}))
-	// .pipe(gulp.dest('fonts/icons/'))
-	.pipe(iconfont({
-		fontName: fontName,
-		startUnicode: false,
-		prependUnicode: false,
-		fontHeight: 1001,
-		normalize: true,
-		formats: ['ttf', 'eot', 'woff', 'svg', 'woff2'],
-	}))
-	.pipe(gulp.dest('fonts/iconsfont/'));
-});
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
-	gulp.watch('scss/**/*.scss', ['sass']);
-	gulp.watch('*.html', browserSync.reload);
-	gulp.watch('js/**/*.js', browserSync.reload);
-});
+function watch() {
+    gulp.watch(paths.styles.src, styles).on('change', browserSync.reload);
+    // gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.html.src).on('change', browserSync.reload);
+}
+
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        notify: false
+    });
+    watch();
+}
+
+
+
+// exports.styles = styles;
+// exports.scripts = scripts;
+// exports.watch = watch;
+exports.serve = serve;
